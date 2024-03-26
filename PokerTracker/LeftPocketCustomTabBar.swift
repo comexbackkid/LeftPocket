324e7350cf02d3165a57aa19ba8233158194630e
Christian <chris.nachtrieb@gmail.com>
2024-03-26 15:48:04 -0400

    Fixed bug with RevenueCat sheet. Wasn't appearing after reaching 25 sessions because the .sheet was linked to the old tabBar.

--- PokerTracker/LeftPocketCustomTabBar.swift
+++ PokerTracker/LeftPocketCustomTabBar.swift
@@ -83,66 +83,66 @@ struct LeftPocketCustomTabBar: View {
     }
     
     // TODO: Delete once fully test tabBarNew
-    var tabBar: some View {
-        
-        HStack {
-            ForEach(0..<5) { index in
-                
-                Button {
-            
-                    if index == 2 {
-                        let impact = UIImpactFeedbackGenerator(style: .medium)
-                        impact.impactOccurred()
-                        
-                        Task {
-                            await AddSessionTip.sessionCount.donate()
-                        }
-                        
-                        // If user is NOT subscribed, AND they reach the 25 Session limit, the Plus button will display Paywall
-                        if !subManager.isSubscribed && viewModel.sessions.count > 24 {
-                            
-                            showPaywall = true
-                        } else {
-                            
-                            isPresented = true
-                        }
-                        
-                        return
-                    }
-                    
-                    let impact = UIImpactFeedbackGenerator(style: .soft)
-                    impact.impactOccurred()
-                    
-                    selectedTab = index
-                    
-                } label: {
-                    
-                    Spacer()
-                    
-                    Image(systemName: tabBarImages[index])
-                        .font(.system(size: index == 2 ? 30 : 22, weight: .black))
-                        .foregroundColor(selectedTab == index ? .brandPrimary : Color(.systemGray4) )
-                    
-                    Spacer()
-                }
-                .sheet(isPresented: $showPaywall) {
-                    PaywallView(fonts: CustomPaywallFontProvider(fontName: "Asap"))
-                        .dynamicTypeSize(.medium...DynamicTypeSize.xLarge)
-                }
-                .task {
-                    for await customerInfo in Purchases.shared.customerInfoStream {
-                        showPaywall = showPaywall && customerInfo.activeSubscriptions.isEmpty
-                        await subManager.checkSubscriptionStatus()
-                    }
-                }
-            }
-        }
-        .padding(.top)
-        .background(.thickMaterial)
-        .sheet(isPresented: $isPresented) {
-            AddNewSessionView(isPresented: $isPresented)
-        }
-    }
+//    var tabBar: some View {
+//        
+//        HStack {
+//            ForEach(0..<5) { index in
+//                
+//                Button {
+//            
+//                    if index == 2 {
+//                        let impact = UIImpactFeedbackGenerator(style: .medium)
+//                        impact.impactOccurred()
+//                        
+//                        Task {
+//                            await AddSessionTip.sessionCount.donate()
+//                        }
+//                        
+//                        // If user is NOT subscribed, AND they reach the 25 Session limit, the Plus button will display Paywall
+//                        if !subManager.isSubscribed && viewModel.sessions.count > 24 {
+//                            
+//                            showPaywall = true
+//                        } else {
+//                            
+//                            isPresented = true
+//                        }
+//                        
+//                        return
+//                    }
+//                    
+//                    let impact = UIImpactFeedbackGenerator(style: .soft)
+//                    impact.impactOccurred()
+//                    
+//                    selectedTab = index
+//                    
+//                } label: {
+//                    
+//                    Spacer()
+//                    
+//                    Image(systemName: tabBarImages[index])
+//                        .font(.system(size: index == 2 ? 30 : 22, weight: .black))
+//                        .foregroundColor(selectedTab == index ? .brandPrimary : Color(.systemGray4) )
+//                    
+//                    Spacer()
+//                }
+//                .sheet(isPresented: $showPaywall) {
+//                    PaywallView(fonts: CustomPaywallFontProvider(fontName: "Asap"))
+//                        .dynamicTypeSize(.medium...DynamicTypeSize.xLarge)
+//                }
+//                .task {
+//                    for await customerInfo in Purchases.shared.customerInfoStream {
+//                        showPaywall = showPaywall && customerInfo.activeSubscriptions.isEmpty
+//                        await subManager.checkSubscriptionStatus()
+//                    }
+//                }
+//            }
+//        }
+//        .padding(.top)
+//        .background(.thickMaterial)
+//        .sheet(isPresented: $isPresented) {
+//            AddNewSessionView(isPresented: $isPresented)
+//        }
+//    }
     
     var tabBarNew: some View {
         
@@ -235,6 +235,7 @@ struct LeftPocketCustomTabBar: View {
                         
                         // If user is NOT subscribed, AND they reach the 25 Session limit, the Plus button will display Paywall
                         if !subManager.isSubscribed && viewModel.sessions.count > 24 {
+                            
                             showPaywall = true
                             
                         } else {
@@ -246,6 +247,16 @@ struct LeftPocketCustomTabBar: View {
                         
                         return
                         
+                    }
+                    .sheet(isPresented: $showPaywall) {
+                        PaywallView(fonts: CustomPaywallFontProvider(fontName: "Asap"))
+                            .dynamicTypeSize(.medium...DynamicTypeSize.xLarge)
+                    }
+                    .task {
+                        for await customerInfo in Purchases.shared.customerInfoStream {
+                            showPaywall = showPaywall && customerInfo.activeSubscriptions.isEmpty
+                            await subManager.checkSubscriptionStatus()
+                        }
                     }
                 }
             }
