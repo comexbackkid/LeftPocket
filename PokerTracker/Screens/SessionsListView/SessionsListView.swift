f915a015b9160b0ffd4806ed8cf3fef3cd83162d
Christian <chris.nachtrieb@gmail.com>
2025-01-01 10:05:14 -0500

    [Clean] Fixed conflict with var screenTitle as result of merge

--- PokerTracker/Screens/SessionsListView/SessionsListView.swift
+++ PokerTracker/Screens/SessionsListView/SessionsListView.swift
@@ -338,12 +338,6 @@ struct SessionsListView: View {
     
     var screenTitle: some View {
         
-        Text(listFilter == .sessions ? sessionsTitle : "All Transactions")
-            .titleStyle()
-            .padding(.horizontal)
-            .listRowBackground(Color.brandBackground)
-            .listRowSeparator(.hidden)
-            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
         HStack (alignment: .center) {
             Text(listFilter == .sessions ? sessionsTitle : "All Transactions")
                 .titleStyle()
