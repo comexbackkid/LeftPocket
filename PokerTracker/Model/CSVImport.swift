8155bc53022b5241ded2a6830e4b18cfc9a832d9
Christian <chris.nachtrieb@gmail.com>
2025-01-15 14:59:24 -0500

    [Feature] Added limit to Poker Bankroll Tracker's import along with Tournament finish

--- PokerTracker/Model/CSVImport.swift
+++ PokerTracker/Model/CSVImport.swift
@@ -172,14 +172,14 @@ class CSVImporter {
             if columns.count == 44 {
                 
                 // Extract only relevant data and create a PokerSession object
-                let game = columns[5].trimmingCharacters(in: .init(charactersIn: "\""))
+                let limit = columns[6].trimmingCharacters(in: .init(charactersIn: "\""))
+                let game = limit + " " + columns[5].trimmingCharacters(in: .init(charactersIn: "\""))
                 let location = LocationModel(name: columns[7].trimmingCharacters(in: .init(charactersIn: "\"")), localImage: "", imageURL: "")
                 let stakesPart1 = columns[20]
                 let stakesPart2 = columns[21]
                 let stakes = "\(stakesPart1)/\(stakesPart2)"
                 let date = convertToDate(columns[0].trimmingCharacters(in: .init(charactersIn: "\"")))
                 let profit = columns[11]
-//                let notes = columns[39].trimmingCharacters(in: .init(charactersIn: "\""))
                 let startTime = convertToDate(columns[0].trimmingCharacters(in: .init(charactersIn: "\"")))
                 let endTime = convertToDate(columns[1].trimmingCharacters(in: .init(charactersIn: "\"")))
                 let expenses = Int(columns[27])
@@ -188,6 +188,7 @@ class CSVImporter {
                 
                 // Tournament Data
                 let sessionType = columns[4].trimmingCharacters(in: .init(charactersIn: "\""))
+                let finish = Int(columns[30])
                 let entrants = Int(columns[31]) ?? 0
                 let tournamentBuyIn = Int(columns[9]) ?? 0
                 
@@ -203,7 +204,7 @@ class CSVImporter {
                                            expenses: sessionType == "Tournament" ? tournamentBuyIn : expenses,
                                            isTournament: sessionType == "Tournament" ? true : false,
                                            entrants: entrants,
-                                           finish: nil,
+                                           finish: finish,
                                            highHandBonus: nil,
                                            buyIn: buyIn,
                                            cashOut: cashOut,
