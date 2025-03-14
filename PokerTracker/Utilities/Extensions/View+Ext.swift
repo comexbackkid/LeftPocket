a12f02aaf1ffa2849beab604153f6e03e7e6ad9b
Christian <chris.nachtrieb@gmail.com>
2025-03-13 21:05:56 -0400

    [Tweak] Just cleaned up a little code & func we don't need

--- PokerTracker/Utilities/Extensions/View+Ext.swift
+++ PokerTracker/Utilities/Extensions/View+Ext.swift
@@ -30,21 +30,4 @@ extension View {
         self
             .shadow(color: colorScheme == .dark ? Color(.clear) : Color(.lightGray).opacity(0.25), radius: 12, x: 0, y: 0)
     }
-    
-    func snapshot() -> UIImage? {
-        let controller = UIHostingController(rootView: self.ignoresSafeArea().fixedSize(horizontal: true, vertical: true))
-        
-        guard let view = controller.view else { return nil }
-
-        let targetSize = view.intrinsicContentSize // to capture entire scroll content
-        if targetSize.width <= 0 || targetSize.height <= 0 { return nil }
-
-        view.bounds = CGRect(origin: .zero, size: targetSize)
-        view.backgroundColor = .clear
-
-        let renderer = UIGraphicsImageRenderer(size: targetSize)
-        return renderer.image { rendereContext in
-            view.drawHierarchy(in: controller.view.bounds, afterScreenUpdates: true)
-        }
-    }
 }
